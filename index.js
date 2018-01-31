8674dca957ce537864665ca24c49ec569d3c8636
Dylan On <dylanon@users.noreply.github.com>
2018-01-31 12:08:48 -0500

    Connect to example Postgres db

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const cool = require('cool-ascii-faces');
+const pg = require('pg');
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
@@ -15,5 +16,16 @@ express()
     for (let i = 0; i < times; i++)
       result += i + ' ';
     res.send(result);
+  })
+  .get('/db', function (req, res) {
+    pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+      client.query('SELECT * FROM test_table', function(err, result) {
+        done();
+        if (err)
+         { console.error(err); res.send("Error " + err); }
+        else
+         { res.render('pages/db', {results: result.rows} ); }
+      });
+    });
   })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
