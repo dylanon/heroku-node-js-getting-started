0209e059dc65ea3dd1625b9de648ab7812ce360b
Dylan On <dylanon@users.noreply.github.com>
2018-01-31 12:03:46 -0500

    Add example route that uses a config var

--- index.js
+++ index.js
@@ -9,4 +9,11 @@ express()
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .get('/cool', (req, res) => res.send(cool()))
+  .get('/times', function (req, res) {
+    let result = ''
+    const times = process.env.TIMES || 5
+    for (let i = 0; i < times; i++)
+      result += i + ' ';
+    res.send(result);
+  })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
